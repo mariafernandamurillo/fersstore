61f9d6003c45a2221fd0f80c6b1c9261ab7d6344
Invito Digital <mariafernandamurillo>
2023-03-12 18:51:11 -0700

    Test 107

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './component/footer';
 import Navbar from './component/navbar'; /*It is imported because the 
 export default line at the bottom of the navbar.jsx*/
+import QuantityPicker from './component/quantityPicker';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Hello from React!</h1>
       <Navbar></Navbar>
       <Footer></Footer>
+      <QuantityPicker></QuantityPicker>
     </div>
   );
 }
