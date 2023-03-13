a73d5c5cba07461c40160819e36760c0837135f5
Invito Digital <mariafernandamurillo>
2023-03-12 18:49:00 -0700

    test 107

---
+++ src/component/quantityPicker.jsx
@@ -0,0 +1,38 @@
+import "./quantityPicker.css";
+import { useState } from "react";
+
+function QuantityPicker(){
+
+    //Create an state variable
+    //Name = quantity
+    //Funtion to modify the variable = setQuantity (Mutable = No achange)
+    let [quantity, setQuantity] = useState(1);
+
+    //Add the logic
+    function decrease(){
+       //console.log("Decrease");
+        if(quantity===1) return; //Avoid negative values
+        let val = quantity - 1; //Decrease de quantity
+        setQuantity(val); //Set the value
+    }
+
+    function increase(){
+        console.log("Increase");
+        let val = quantity + 1; //Increase one by one
+        setQuantity(val); //Add the value
+    }
+
+    return (
+        //Add the HTML elements
+        <div>
+            <button onClick={decrease}>-</button>
+
+            {/* Show the quantity in the label */}   
+            <label>{quantity}</label>
+            
+            <button onClick={increase}>+</button>
+        </div>
+    )
+}
+
+export default QuantityPicker;
